e76b6886479fba7f9bf192cd9c9af6d8b983a1e4
sumanthkv-bmsce <sumanth.cs18@bmsce.ac.in>
2020-08-03 13:22:53 +0530

    media1

--- src/App.js
+++ src/App.js
@@ -1,17 +1,19 @@
 import React from 'react';
 import { Navbar,NavbarBrand } from 'reactstrap';
 import logo from './logo.svg';
+import Menu from './components/MenuComponent'
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
+    <div>
         <Navbar dark color="primary">
 
           <div className="container">
               <NavbarBrand href="/">Ristrante confusion</NavbarBrand>
           </div>
         </Navbar>
+        <Menu></Menu>
 
     </div>
   );
